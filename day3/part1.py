3d171e97211e2026c1ac0f441f9ee9cc9206eb35
Zoe Lee <zoe@kihira.uk>
2017-12-08 15:33:56 +0100

    More attempts at day 3

--- day3/part1.py
+++ day3/part1.py
@@ -1,12 +1,36 @@
-from math import sqrt
-
 num = int(input("Input: "))
 count = 0
+squareLength = 3
 
-while not sqrt(num).is_integer():
-    num -= 1
-    count += 1
+while squareLength * squareLength < num:
+    squareLength += 2
+squareLength -= 2
+cornerValue = squareLength * squareLength
+diff = num - cornerValue
 
-print(num)
-print(count)
-print(f"Steps: {format(sqrt(num) - 1 + count)}")
+midPoint = (squareLength + 1) / 2
+# # Walk right one
+# diff += 1
+# # Walk north
+# if squareLength > diff:
+#     diff = diff - midPoint
+# else:
+#     diff -= squareLength
+# # Walk west
+# if squareLength + 1 > diff:
+#     diff = diff - midPoint
+# else:
+#     diff -= squareLength + 1
+# # Walk South
+# if squareLength + 1 > diff:
+#     diff = diff - midPoint
+# else:
+#     diff -= squareLength + 1
+# # Walk East
+# if squareLength + 1 > diff:
+#     diff = diff - midPoint
+
+print(squareLength)
+print(cornerValue)
+print(diff)
+print(midPoint)
