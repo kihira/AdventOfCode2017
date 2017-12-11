d60d4cb6d43e128be4948264f378d88c724e2b4f
Zoe Lee <zoe@kihira.uk>
2017-12-11 14:03:13 +0100

    Day 5 part 1

---
+++ day5/part1.py
@@ -0,0 +1,13 @@
+with open("part1.txt", "r") as file:
+    offsets = []
+    for row in file:
+        offsets.append(int(row.strip()))
+
+    position = 0
+    steps = 0
+    while position < len(offsets):
+        instruction = offsets[position]
+        offsets[position] += 1
+        position += instruction
+        steps += 1
+    print(steps)
